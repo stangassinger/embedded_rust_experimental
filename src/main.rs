2d22458fca8e8ae4ad441c09a3ee8e02301fefd2
Guenther <guenther.stangassinger@gmx.de>
2019-07-29 18:18:52 +0000

    compiling :-)

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,25 @@ use stm32f4xx_hal::{delay, gpio, stm32, timer};
 extern crate stm32f4;
 extern crate panic_halt;
 
+
 #[app(device = stm32f4::stm32f429 )]
+
 const APP: () = {
-    static mut TIMER: stm32f4xx_hal::timer::Timer<stm32::TIM3> = ();
+    #[init]
+    fn init(){
+        static mut X: u32 = 0;
 
 
-
-    #[init()]
-    fn init() -> init::LateResources {
-        let mut flash = device.FLASH;
-        
-        let mut rcc   = device.RCC.constrain();
-       
-        let clocks = rcc
-            .cfgr
-            .use_hse(20.mhz())
-            .sysclk(168.mhz())
-            .pclk1(42.mhz())
-            .pclk2(84.mhz())           
-            .freeze();
-        let mut gpiog = device.GPIOG.split();
-        let led13 = gpiog.pg13.into_push_pull_output();        
+        // Safe access to local `static mut` variable
+        let _x: &'static mut u32 = X;
 
         
     }
-
-
-
-
-
-
-    // Interrupt handlers used to dispatch software tasks
-    extern "C" {
-        fn EXTI2();
-    }
+    
+   // Interrupt handlers used to dispatch software tasks
+   extern "C" {
+       fn EXTI2();
+   }   
+    
+    
 };

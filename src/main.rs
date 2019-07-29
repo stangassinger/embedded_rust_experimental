1da44d51ae0cfee74d33f8314413694d03ea5292
guenther stangassinger <guenther.stangassinger@gmx.de>
2019-07-29 20:01:15 +0000

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![no_main]
 #![no_std]
-
 #[cfg(not(test))]
 
+extern crate stm32f4;
+extern crate panic_halt;
 
 
 use rtfm::app;
@@ -10,12 +11,8 @@ use stm32f4xx_hal::prelude::*;
 use stm32f4xx_hal::{delay, gpio, stm32, timer};
 
 
-extern crate stm32f4;
-extern crate panic_halt;
-
 
 #[app(device = stm32f4::stm32f429 )]
-
 const APP: () = {
     #[init]
     fn init(){

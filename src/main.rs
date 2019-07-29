69c84423cca6acc62f7280cd8fe4e5bdf0a7cd39
guenther stangassinger <guenther.stangassinger@gmx.de>
2019-07-29 20:51:27 +0000

    ...

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,34 @@
 
 extern crate stm32f4;
 extern crate panic_halt;
-
+extern crate cortex_m_semihosting;
 
 use rtfm::app;
 use stm32f4xx_hal::prelude::*;
 use stm32f4xx_hal::{delay, gpio, stm32, timer};
 
 
+use cortex_m_semihosting::{debug, hprintln};
+
 
 #[app(device = stm32f4::stm32f429 )]
 const APP: () = {
+
+  
+ 
+
+
+
     #[init]
     fn init(){
         static mut X: u32 = 0;
 
-
+        hprintln!("Hello, world!").unwrap();
         // Safe access to local `static mut` variable
         let _x: &'static mut u32 = X;
+        
+        
+        
 
         
     }
